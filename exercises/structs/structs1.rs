dda9151a6a62b49bf654fbc62fb078b226b4684a
杨如丹 <yangrudan@zhejianglab.com>
2024-11-27 14:10:41 +0800

    finish struct && check score

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -2,10 +2,7 @@
 //
 // Address all the TODOs to make the tests pass!
 //
-// Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
+// Execute `rustlings hint structs1` or use the `hint` watch subcommand for a hint.
 
 struct ColorClassicStruct {
     // TODO: Something goes here
