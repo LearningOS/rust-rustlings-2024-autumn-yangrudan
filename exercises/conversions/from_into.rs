69da3e0efe742f83542c015e7f7aa612452a6163
yangrudan <1549029765@qq.com>
2024-11-28 20:19:24 +0800

    finish clippy && conversation

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,24 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut parts = s.split(',');
+        if parts.clone().collect::<Vec<_>>().len() != 2 {
+            return Person::default();
+        }
+        let name = parts.next().unwrap_or_default();
+        let age = parts.next().unwrap_or_default().parse::<usize>().unwrap_or_default();
+        if name.is_empty() {
+            return Person::default();
+        }
+        if age == 0 {
+            return  Person::default();
+        }
+        
+        Person{name: name.to_string(), age}
+
     }
 }
 
